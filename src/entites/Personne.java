022d083b07eb73133b814de07b938e862c4a1ee6
Otmane BOUJLAM <otmane.boujlam@gmail.com>
2023-04-12 10:30:44 +0200

    TP:3

--- src/entites/Personne.java
+++ src/entites/Personne.java
@@ -4,15 +4,43 @@ public class Personne {
 	String nom;
 	String prenom;
 	AdressePostale adresse;
-	
+
 	Personne(String nvNom, String nvPrenom, AdressePostale nvAdresse) {
 		nom = nvNom;
 		prenom = nvPrenom;
 		adresse = nvAdresse;
 	}
-	
+
 	Personne(String nvNom, String nvPrenom) {
 		nom = nvNom;
 		prenom = nvPrenom;
+	}
+
+	void showFullName() {
+		System.out.println(nom.toUpperCase() + " " + prenom);
+	}
+
+	void setNom(String nvNom) {
+		nom = nvNom;
+	}
+
+	void setPrenom(String nvPrenom) {
+		prenom = nvPrenom;
+	}
+
+	void setAdresse(AdressePostale nvAdresse) {
+		adresse = nvAdresse;
+	}
+
+	String getNom() {
+		return nom;
+	}
+
+	String getPrenom() {
+		return prenom;
+	}
+
+	AdressePostale getAdresse() {
+		return adresse;
 	}
 }
--- src/entites/TestPersonne.java
+++ src/entites/TestPersonne.java
@@ -3,12 +3,10 @@ package entites;
 public class TestPersonne {
 
 	public static void main(String[] args) {
-		
+
+		Personne p1 = new Personne("BOUJLAM", "Otmane");
 		AdressePostale adr1 = new AdressePostale(1, "avenue du President Wilson", 34500, "Beziers");
-		Personne p1 = new Personne("BOUJLAM", "Otmane", adr1);
-		
-		
-		Personne p2 = new Personne("COUSCOUS", "Boulette");
+		p1.setAdresse(adr1);
 	}
 
 }
