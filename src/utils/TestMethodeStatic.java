4a26f1e5bcdca685b5b043d07d4fb9fe7d590b3f
Otmane BOUJLAM <otmane.boujlam@gmail.com>
2023-04-12 12:12:03 +0200

    TP:4

---
+++ src/utils/TestMethodeStatic.java
@@ -0,0 +1,14 @@
+package utils;
+
+public class TestMethodeStatic {
+
+	public static void main(String[] args) {
+		String chaine = "12";
+		int entier = Integer.parseInt(chaine);
+		int a = 2;
+		int b = 3;
+		int max = Integer.max(a, b);
+		System.out.println(max);
+	}
+
+}
